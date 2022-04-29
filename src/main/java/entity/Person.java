beff5494660c26189e829700c316f36db9f357b8
HosseinJavooni <h.javooni@gmail.com>
2022-04-29 10:51:21 +0430

    first_commit_14010209_1050

---
+++ src/main/java/entity/Person.java
@@ -0,0 +1,22 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PERSON")
+public class Person {
+
+    @Id
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+}
